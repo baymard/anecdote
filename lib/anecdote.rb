355c4b3b3bbb3ff897df953ff0c9a6f0b4c880df
Jamie Appleseed <jamie@baymard.com>
2016-02-11 12:10:04 +0100

    Custom float and flow breakpoints + Embedded content (e.g. videos) in graphics

--- lib/anecdote.rb
+++ lib/anecdote.rb
@@ -25,19 +25,23 @@ module Anecdote
         contents = []
         contents << view_context.content_tag((settings[:href].present? ? :a : :div), (settings[:href].present? ? { href: settings[:href], title: settings[:href_title] } : {}).merge({ class: 'anecdote-intrinsic-embed-n42ha1' })) do
           if settings[:assets_path]
-            image_url = settings[:assets_path]
+            content = view_context.image_tag(settings[:assets_path], alt: '')
             paperclip_geo = Paperclip::Geometry.from_file(Rails.root.join('app', 'assets', 'images', settings[:assets_path]))
             geo = { width: paperclip_geo.width, height: paperclip_geo.height }
           elsif settings[:image_url]
-            image_url = settings[:image_url]
-            dimensions = settings[:image_dimensions].split('x').map(&:to_f)
+            content = view_context.image_tag(settings[:image_url], alt: '')
+            dimensions = (settings[:dimensions] || settings[:image_dimensions]).split('x').map(&:to_f)
+            geo = { width: dimensions.first, height: dimensions.last }
+          elsif settings[:embed_code]
+            content = settings[:embed_code].html_safe
+            dimensions = settings[:dimensions].split('x').map(&:to_f)
             geo = { width: dimensions.first, height: dimensions.last }
           end
           if settings[:_scope_].present? && settings[:_scope_][:processor].tag == 'gallery'
             settings[:_scope_][:settings][:_graphics_] ||= []
             settings[:_scope_][:settings][:_graphics_] << geo
           end
-          view_context.content_tag(:div, view_context.image_tag(image_url, alt: ''), class: 'inner', style: "padding-bottom: #{geo[:height] / geo[:width] * 100}%;")
+          view_context.content_tag(:div, content, class: 'inner', style: "padding-bottom: #{geo[:height] / geo[:width] * 100}%;")
         end
         if settings[:caption].present?
           contents << view_context.content_tag(:div, view_context.content_tag(:div, markdown_and_parse(settings[:caption]), class: 'inner anecdote-wysicontent-ndj4ab'), class: 'anecdote-caption-ajkd3b')
@@ -109,6 +113,28 @@ module Anecdote
           "style=\"#{styles.join(';')}\" #{match}".html_safe
         end
 
+        # flow-points
+        flow_from_klass = case settings[:flow_from]
+        when 'always' then 'v-flow-from-always'
+        when 'medium-handheld' then 'v-flow-from-medium-handheld'
+        when 'large-handheld' then 'v-flow-from-large-handheld'
+        when 'tablet' then 'v-flow-from-tablet'
+        when 'laptop' then 'v-flow-from-laptop'
+        when 'large-monitor' then 'v-flow-from-large-monitor'
+        else
+          if settings[:size].present?
+            case settings[:size]
+            when 'small' then 'v-flow-from-medium-handheld'
+            when 'medium' then 'v-flow-from-large-handheld'
+            when 'big' then 'v-flow-from-tablet'
+            when 'full-width' then 'v-flow-from-tablet'
+            end
+          else
+            'v-flow-from-tablet' # default
+          end
+        end
+        klass += " #{flow_from_klass}"
+
         # build HTML output
         contents = []
         contents << view_context.content_tag(:div, graphics.html_safe, class: 'content')
@@ -141,6 +167,24 @@ module Anecdote
       klasses << case settings[:float]
       when 'right' then 'v-float-right'
       when 'left' then 'v-float-left'
+      end
+      klasses << case settings[:float_from]
+      when 'always' then 'v-float-from-always'
+      when 'medium-handheld' then 'v-float-from-medium-handheld'
+      when 'large-handheld' then 'v-float-from-large-handheld'
+      when 'tablet' then 'v-float-from-tablet'
+      when 'laptop' then 'v-float-from-laptop'
+      when 'large-monitor' then 'v-float-from-large-monitor'
+      else
+        if settings[:size].present?
+          case settings[:size]
+          when 'small' then 'v-float-from-large-handheld'
+          when 'medium' then 'v-float-from-laptop'
+          when 'big' then 'v-flow-from-large-monitor'
+          end
+        else
+          'v-float-from-laptop' # default
+        end
       end
     end
     if settings[:mood].present?
