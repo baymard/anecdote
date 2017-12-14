753d5e85dffd4f351ab2ce34a5e8b3bf0b7eb1a8
Jamie Appleseed <jamieappleseed@gmail.com>
2017-12-14 12:33:28 +0100

    Rails upgrade fix

--- test/dummy/app/controllers/pages_controller.rb
+++ test/dummy/app/controllers/pages_controller.rb
@@ -2,6 +2,6 @@ class PagesController < ApplicationController
   def show
     page = params[:id] || 'article'
     file = File.read Rails.root.join('app', 'views', 'pages', "#{page}.markdown")
-    render text: Anecdote.markdown_and_parse(file), layout: 'application'
+    render html: Anecdote.markdown_and_parse(file), layout: 'application'
   end
 end
